532aecdc6782518cde05c9b65f50fb5f19448e0a
inokinsy <inokinsy@gmail.com>
2022-07-01 08:49:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 /**
  * main - Entry point
  *  * Return: Always 0 (sucess)
- *   */
- int main(void)
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
