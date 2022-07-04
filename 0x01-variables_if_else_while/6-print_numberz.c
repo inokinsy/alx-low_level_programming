092b0a6e3c72086a95a4384ebe8eb63664659976
inokinsy <inokinsy@gmail.com>
2022-07-04 10:10:26 -0700

    my commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
-*main - Entry point
-*Return 0 if successful
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
  */
 
 int main(void)
+
 {
 int ch;
-for (ch = 0; ch <= 9; ch++)
+for (ch = 48; ch <= 57; ch++)
 {
 putchar(ch);
 }
