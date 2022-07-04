21dc5ddd4a907abb72d3ecdf0643703dbd27783a
inokinsy <inokinsy@gmail.com>
2022-07-04 09:42:56 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Return 0 if successful
+ */
+
+int main(void)
+{
+int ch;
+for (ch = 0; ch <= 9; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
