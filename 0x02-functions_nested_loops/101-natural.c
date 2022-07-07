6245a8ff4e6232cfe6eb9f15ab242b1063280a63
inokinsy <inokinsy@gmail.com>
2022-07-06 22:30:13 -0700

    my commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 
 int main(void)
+
 {
 int ii, _sum;
 for (ii = 3; ii < 1024; ii++)
@@ -15,7 +17,5 @@ if (ii % 3 == 0 || ii % 5 == 0)
 _sum += ii;
 }
 printf("%d\n", _sum);
-return (0);
-}
 return (0);
 }
