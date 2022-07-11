562ceaa73ce7f9882b3cab693242f52b80679361
inokinsy <inokinsy@gmail.com>
2022-07-11 11:06:46 -0700

    my commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,6 +6,7 @@
   */
 
 void print_times_table(int n)
+
 {
 	int i, j, k;
 
@@ -45,7 +46,7 @@ void print_times_table(int n)
 				_putchar((k % 10) + '0');
 			}
 			}
-			_putchar('\n');
+		_putchar('\n');
 	}
 	}
 }
