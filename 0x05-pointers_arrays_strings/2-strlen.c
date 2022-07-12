ffde580edff9b9d8d29e0a91353c319026f5aae9
inokinsy <inokinsy@gmail.com>
2022-07-12 04:52:07 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+#include "main.h"
+/**
+ * _strlen - prints the length of a string
+ * @s: string variable
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+
+{
+
+		return (strlen(s));
+
+}
