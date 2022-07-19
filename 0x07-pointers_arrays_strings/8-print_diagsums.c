ea31a0147fb4e52ea049f31ca1594994a1a402e9
inokinsy <inokinsy@gmail.com>
2022-07-18 19:47:33 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,25 +5,26 @@
  *  print_diagsums - print the sum of leading diagonal
  *  @a: pointer to the array
  *  @size: size of the array
+ *  @size: size of square matrix
+ *  return: zero
  */
 
 void print_diagsums(int *a, int size)
 {
-		int i, sumDiag = 0, sumTrailDiag = 0;
+	int i, j;
+	unsigned int sum1, sum2;
 
-for (i = 0; i < size * size; i++)
-
-if ((i % (size + 1) == 0) && (i % (size - 1) == 0) && i && i < size * size - 2)
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
 		{
-			sumDiag += a[i];
-			sumTrailDiag += a[i];
-		}
-		else if (i % (size + 1) == 0)
-			sumDiag += a[i];
-		else if (i % (size - 1) == 0 && i && i < size * size - 2)
-			sumTrailDiag += a[i];
-			}
+			if (i == j)
+			sum1 += a[i][j];
 
-			printf("%d, %d", sumDiag, sumTrailDiag);
-			putchar('\n');
+			if ((i + j) == (size - 1))
+				sum2 += a[i][j];
+		}
+	}
+	printf("%u, %u", sum1, sum2);
 }
+
