7526d43188bd510856380b41ac6df11e0206520b
inokinsy <inokinsy@gmail.com>
2022-07-18 19:12:16 -0700

    my comiit task o - 8

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+
+/**
+ *  _memcpy - fills the first n bytes of the memory of the
+ *   memory area pointed to by s with constant byte b
+ *   @dest: pointer to the string dest
+ *   @src: pointer to the  string src
+ *   @n: size in byte of a memory space
+ *   Return: pointer to the variable dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n);
+	return (dest);
+}
+
---
+++ 0x07-pointers_arrays_strings/22-strchr.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ *  _strchr - find a character in a string
+ *   @s: pointer to the string s to be searched
+ *    @c: character to be found
+ *    Return: pointer to the first occurence of the character c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	if (s[i] == c)
+	return (s + i);
+			}
